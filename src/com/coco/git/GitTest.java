bfc3e7e9f1ca1a03a8c2bc0a25d9e8825b3b1140
Coco <YuriMeow@mail.dlut.edu.cn>
2023-12-17 17:58:14 +0800

    second commit

--- src/com/coco/git/GitTest.java
+++ src/com/coco/git/GitTest.java
@@ -12,5 +12,6 @@ package com.coco.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
