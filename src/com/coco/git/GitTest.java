b01236dee83603e05dec247a544a06226bf706b5
Coco <YuriMeow@mail.dlut.edu.cn>
2023-12-17 17:54:15 +0800

    first commit

---
+++ src/com/coco/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.coco.git;
+
+/**
+ * ClassName:GitTest
+ * Package:com.coco.git
+ * Description:
+ *
+ * @Author Coco
+ * @Create 2023/12/17 16:58
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
